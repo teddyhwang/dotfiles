56ca98326faae8fbc69eaee62ea9a96546c653db
Teddy Hwang <tshwang@gmail.com>
2023-01-07 00:39:28 -0500

    fix conflicting keymap

--- home/config/nvim/lua/teddyhwang/core/keymaps.lua
+++ home/config/nvim/lua/teddyhwang/core/keymaps.lua
@@ -38,7 +38,7 @@ keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available 
 
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>b", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
 keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
 
 keymap.set("n", "<leader>R", ":LspRestart<CR>") -- mapping to restart lsp if necessary

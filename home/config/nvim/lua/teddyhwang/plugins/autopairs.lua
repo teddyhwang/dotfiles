6bb43cb33d96442db65f88635e96500911fcc95c
Teddy Hwang <tshwang@gmail.com>
2023-01-08 23:44:23 -0500

    better integrate cmp with copilot

--- home/config/nvim/lua/teddyhwang/plugins/autopairs.lua
+++ home/config/nvim/lua/teddyhwang/plugins/autopairs.lua
@@ -10,10 +10,6 @@ end
 
 autopairs.setup({
   check_ts = true, -- enable treesitter
-  ts_config = {
-    lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-    javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-  },
 })
 
 local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }

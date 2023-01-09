90bf79d3fa8dabf0076455890b4a12d5dbdf0c29
Teddy Hwang <teddy.hwang@shopify.com>
2023-01-09 09:33:52 -0500

    add additional keyboard shortcut for lsp references

--- home/config/nvim/lua/teddyhwang/plugins/lsp/lspconfig.lua
+++ home/config/nvim/lua/teddyhwang/plugins/lsp/lspconfig.lua
@@ -19,6 +19,7 @@ local on_attach = function(client, bufnr)
   local opts = { noremap = true, silent = true, buffer = bufnr }
 
   vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+  vim.keymap.set("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
   vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
   vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
   vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation

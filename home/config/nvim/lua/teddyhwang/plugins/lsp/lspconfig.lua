23200f1caf96c0ca20f4ef02f0ed65ee1d88f75c
Teddy <tshwang@gmail.com>
2023-02-21 19:56:53 -0500

    use lspsaga for references

--- home/config/nvim/lua/teddyhwang/plugins/lsp/lspconfig.lua
+++ home/config/nvim/lua/teddyhwang/plugins/lsp/lspconfig.lua
@@ -25,12 +25,12 @@ vim.keymap.set("n", "<leader>R", ":LspRestart<cr>")
 local on_attach = function(client, bufnr)
   local opts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set("n", "gr", "<cmd>References<cr>", opts) -- show references
-  -- vim.keymap.set("n", "gr", "zt<cr><cmd>Lspsaga lsp_finder<cr>", opts) -- show definition, references
+  vim.keymap.set("n", "gr", "zz<cmd>Lspsaga lsp_finder<cr>", opts) -- show definition, references
+  -- vim.keymap.set("n", "gr", "<cmd>References<cr>", opts) -- show references
   -- vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts) -- show references
   vim.keymap.set("n", "gd", "<cmd>Definitions<cr>", opts) -- see definition and make edits in window
-  -- vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts) -- see definition and make edits in window
   -- vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+  -- vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts) -- see definition and make edits in window
   vim.keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<cr>", opts) -- see definition and make edits in window
   -- vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts) -- got to declaration
   vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts) -- go to implementation

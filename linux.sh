3f76adfcb5835640bbb72de8c14ad4cd14bf6dbc
Teddy Hwang <teddy.hwang@shopify.com>
2023-05-10 11:02:53 -0400

    more brew migration

--- linux.sh
+++ linux.sh
@@ -8,11 +8,11 @@ touch ~/.z
 if ! command -v brew &> /dev/null; then
   BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
   echo -e "${C_GREEN}Installing Brew packages...$C_DEFAULT"
-  $BREW_BIN install tmux git-delta fzf bat fd ranger
+  $BREW_BIN install tmux git-delta fzf bat fd ranger universeal-ctags gzip
   $($BREW_BIN --prefix)/opt/fzf/install --all
 else
   echo -e "${C_LIGHTGRAY}Brew bundled$C_DEFAULT"
 fi
-sudo apt-get install -y xdg-utils highlight universal-ctags pip fd-find
+sudo apt-get install -y xdg-utils highlight fd-find
 
 pip install neovim mycli

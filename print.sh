15a9adbf9204d8979189cee190b8cfdb6c21c69a
Teddy Hwang <tshwang@gmail.com>
2025-01-01 22:50:33 -0500

    remove duplicate color

--- print.sh
+++ print.sh
@@ -5,7 +5,6 @@ export C_GREEN="\x1B[32m"
 export C_RED="\x1B[31m"
 export C_LIGHTGRAY="\x1B[90m"
 export C_ORANGE="\x1B[33m"
-export C_YELLOW="\x1B[33m"
 
 print_success() {
   echo -e "${C_GREEN}$1${C_DEFAULT}"

fde13ea2429a0663fec3a5cd662563629dba3788
teddyhwang <teddy.hwang@shopify.com>
2025-02-16 07:35:22 -0700

    move snacks to dedicated plugin file

---
+++ home/config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,72 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = false },
+    dashboard = { enabled = true },
+    dim = { enabled = true },
+    explorer = { enabled = true },
+    image = { force = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    lazygit = {
+      theme = {
+        activeBorderColor = { fg = "Title", bold = true },
+      },
+    },
+    picker = {
+      formatters = {
+        file = {
+          truncate = 100,
+        },
+      },
+      win = {
+        input = {
+          keys = {
+            ["<a-s>"] = { "flash", mode = { "n", "i" } },
+            ["s"] = { "flash" },
+          },
+        },
+      },
+      actions = {
+        flash = function(picker)
+          require("flash").jump({
+            pattern = "^",
+            label = { after = { 0, 0 } },
+            search = {
+              mode = "search",
+              exclude = {
+                function(win)
+                  return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                end,
+              },
+            },
+            action = function(match)
+              local idx = picker.list:row2idx(match.pos[1])
+              picker.list:_move(idx, true, true)
+            end,
+          })
+        end,
+      },
+    },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    styles = {
+      float = {
+        border = "rounded",
+        backdrop = 40,
+      },
+    },
+    terminal = { enabled = true },
+    toggle = { enabled = true },
+    words = { enabled = true },
+    zen = { enabled = true },
+  },
+  init = function()
+    vim.g.snacks_animate = false
+  end,
+}

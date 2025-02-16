a06f6d74ee022094b79cb76a2378b5fbdb984b35
teddyhwang <teddy.hwang@shopify.com>
2025-02-15 22:24:57 -0700

    add flash to snacks picker

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -79,7 +79,41 @@ return {
           activeBorderColor = { fg = "Title", bold = true },
         },
       },
-      picker = { enabled = true },
+      picker = {
+        formatters = {
+          file = {
+            truncate = 100,
+          },
+        },
+        win = {
+          input = {
+            keys = {
+              ["<a-s>"] = { "flash", mode = { "n", "i" } },
+              ["s"] = { "flash" },
+            },
+          },
+        },
+        actions = {
+          flash = function(picker)
+            require("flash").jump({
+              pattern = "^",
+              label = { after = { 0, 0 } },
+              search = {
+                mode = "search",
+                exclude = {
+                  function(win)
+                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                  end,
+                },
+              },
+              action = function(match)
+                local idx = picker.list:row2idx(match.pos[1])
+                picker.list:_move(idx, true, true)
+              end,
+            })
+          end,
+        },
+      },
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
@@ -142,7 +176,6 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     config = true,
-    keys = {},
   },
   {
     "MagicDuck/grug-far.nvim",

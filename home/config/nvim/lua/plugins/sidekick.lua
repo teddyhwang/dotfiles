fa27d6ec72d88dda02f8d6aa59d817af5d330c6e
teddyhwang <teddy.hwang@shopify.com>
2025-11-06 12:34:05 -0500

    rename to ai for clarity

--- home/config/nvim/lua/plugins/sidekick.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      copilot_node_command = vim.fn.executable("/opt/homebrew/bin/node") == 1 and "/opt/homebrew/bin/node"
-        or "node",
-    },
-  },
-}

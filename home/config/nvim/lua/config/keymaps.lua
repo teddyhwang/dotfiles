270b16c59690257bd51339362045808629957cae
Teddy Hwang <tshwang@gmail.com>
2025-11-09 07:47:26 -0500

    don't create default key maps

--- home/config/nvim/lua/config/keymaps.lua
+++ home/config/nvim/lua/config/keymaps.lua
@@ -149,25 +149,3 @@ map("n", "<leader>D", "<cmd>DBUIToggle<cr>", { desc = "Toggle Dadbod UI" })
 map("n", "<leader>ac", function()
   require("sidekick.cli").toggle({ name = "claude", focus = true })
 end, { desc = "Toggle Claude" })
-
--- LazyVim uses flash.nvim by default, these mappings should work out of the box
--- but we'll define them explicitly to match your config
-map({ "n", "x", "o" }, "s", function()
-  require("flash").jump()
-end, { desc = "Flash" })
-
-map({ "n", "x", "o" }, "S", function()
-  require("flash").treesitter()
-end, { desc = "Flash Treesitter" })
-
-map("o", "r", function()
-  require("flash").remote()
-end, { desc = "Remote Flash" })
-
-map({ "o", "x" }, "R", function()
-  require("flash").treesitter_search()
-end, { desc = "Treesitter Search" })
-
-map("c", "<c-s>", function()
-  require("flash").toggle()
-end, { desc = "Toggle Flash Search" })

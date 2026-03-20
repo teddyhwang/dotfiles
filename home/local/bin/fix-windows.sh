2550c22b675b80f09bea9307f31b4cb974da0f92
Teddy Hwang <tshwang@gmail.com>
2026-03-19 22:07:12 -0400

    update brewfile and scripts to manage windows and audio

---
+++ home/local/bin/fix-windows.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Move Messages, Discord, and Ghostty to screen 2 via Hammerspoon
+/opt/homebrew/bin/hs -c "moveAppsToScreen2()"
---
+++ home/local/bin/restart-wave.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Restart Elgato Wave Link via Hammerspoon
+/opt/homebrew/bin/hs -c "restartWaveLink()"
